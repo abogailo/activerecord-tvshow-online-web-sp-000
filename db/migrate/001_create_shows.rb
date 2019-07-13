b89ee50fd93d552f16e9783670affd5fb1982da1
Abagail Hudson <abagailanne@gmail.com>
2019-07-13 00:26:42 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,2 @@
-class CreateShows < ActiveRecord::Migration[5.2]
-  def change
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
-    end
-  end
+class Show < ActiveRecord::Base
 end
